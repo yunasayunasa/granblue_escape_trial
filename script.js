94b189abb5801b9badda84dc45b43d91ab601249
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-02-15 21:18:52 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("JavaScript 読み込み完了");
+  console.log("JavaScript 読み込み完了");
 
-    document.getElementById("bed").addEventListener("click", function () {
-        alert("ベッドを調べた！");
-    });
+  // ベッドをタップしたら
+  document.getElementById("bed").addEventListener("click", function () {
+    alert("ベッドを調べた！");
+  });
 
-    document.getElementById("caster").addEventListener("click", function () {
-        alert("キャスターを調べた！");
-    });
+  // キャスターをタップしたら
+  document.getElementById("caster").addEventListener("click", function () {
+    alert("キャスターを調べた！");
+  });
 });
